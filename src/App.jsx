201d6954c72b984e0a27f6a03782f838742556ab
Nikhil Ganireddy <nikhilganireddy@yahoo.com>
2023-05-21 17:37:14 +0530

    zdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import UserContextProvider from "./UserContext";
-import {Route, Routes} from "react-router";
-import {Toaster} from "react-hot-toast";
+import { Route, Routes } from "react-router";
+import { Toaster } from "react-hot-toast";
 import HomePage from "./Pages/HomePage";
 import LoginPage from "./Pages/LoginPage.jsx";
 import axios from "axios";
@@ -9,18 +9,22 @@ import Layout from "./Layout.jsx";
 import Dashboard from "./Pages/Dashboard.jsx";
 
 const App = () => {
-    axios.defaults.baseURL = "http://localhost:4000";
-    axios.defaults.withCredentials = true;
-    return (<UserContextProvider>
-        <Toaster position="top-center" toastOptions={{duration: 5000}}/>
-        <Routes>
-            <Route index element={<HomePage/>}/>
-            <Route path={"/login"} element={<LoginPage/>}/>
-            <Route path={"/user"} element={<Layout/>}>
-                <Route path={"/user/dashboard"} element={<Dashboard/>}/>
-            </Route>
-        </Routes>
-    </UserContextProvider>);
+  // axios.defaults.baseURL = "http://localhost:4000";
+  axios.defaults.baseURL = "https://token-server-721u.onrender.com";
+
+  axios.defaults.withCredentials = true;
+  return (
+    <UserContextProvider>
+      <Toaster position="top-center" toastOptions={{ duration: 5000 }} />
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path={"/login"} element={<LoginPage />} />
+        <Route path={"/user"} element={<Layout />}>
+          <Route path={"/user/dashboard"} element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </UserContextProvider>
+  );
 };
 
 export default App;
