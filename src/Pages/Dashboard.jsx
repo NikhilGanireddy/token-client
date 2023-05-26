d48d8926ec1a91fcf63b4050747095e8953e353c
Nikhil Ganireddy <nikhilganireddy@yahoo.com>
2023-05-26 17:32:19 +0530

    netlify

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -20,23 +20,23 @@ const Dashboard = () => {
   const [tempUser, setTempUser] = useState(null);
 
   return (
-    <div className={`dashboard w-full min-h-0 rounded-2xl overflow-y-scroll flex flex-col gap-6 `}>
+    <div
+      className={`dashboard w-full min-h-0 rounded-2xl overflow-y-scroll flex flex-col sm:gap-2 md:gap-3 lg:gap-6 pr-2`}
+    >
       <div className=" grid grid-rows-1">
         <div
-          className={`grid gap-6 sm:grid-cols-1 md:grid-cols-10 sm:grid-rows-3 md:grid-rows-1`}
+          className={`grid sm:gap-2 md:gap-3 lg:gap-6  sm:grid-cols-1 md:grid-cols-10 sm:grid-rows-3 md:grid-rows-1`}
         >
           <div
-            className={`text-3xl sm:col-span-full md:col-span-4 rounded-2xl backdrop-blur-sm min-h-[500px]  ${changeBg()}`}
-          >
-            grid 1
-          </div>
+            className={`text-3xl sm:col-span-full md:col-span-4 rounded-2xl backdrop-blur-sm min-h-[350px]  ${changeBg()}`}
+          ></div>
           <div
-            className={`text-3xl sm:col-span-full md:col-span-3 rounded-2xl backdrop-blur-sm min-h-[500px]  ${changeBg()}`}
+            className={`text-3xl sm:col-span-full md:col-span-3 rounded-2xl backdrop-blur-sm min-h-[350px]  ${changeBg()}`}
           >
             grid 2
           </div>
           <div
-            className={`text-3xl sm:col-span-full md:col-span-3 rounded-2xl backdrop-blur-sm min-h-[500px]  ${changeBg()}`}
+            className={`text-3xl sm:col-span-full md:col-span-3 rounded-2xl backdrop-blur-sm min-h-[350px]  ${changeBg()}`}
           >
             grid 3
           </div>
@@ -44,23 +44,39 @@ const Dashboard = () => {
       </div>
       <div className=" grid grid-rows-1">
         <div
-          className={`grid gap-6 sm:grid-cols-1 md:grid-cols-10 sm:grid-rows-3 md:grid-rows-1`}
+          className={`grid  sm:gap-2 md:gap-3 lg:gap-6  sm:grid-cols-1 md:grid-cols-10 sm:grid-rows-2 md:grid-rows-1`}
         >
           <div
-            className={`text-3xl sm:col-span-full md:col-span-4 rounded-2xl backdrop-blur-sm min-h-[500px]  ${changeBg()}`}
+            className={`text-3xl sm:col-span-full md:col-span-6 rounded-2xl backdrop-blur-sm min-h-[350px]  ${changeBg()}`}
           >
-            grid 1
+            grid 4
           </div>
-          <div
-            className={`text-3xl sm:col-span-full md:col-span-3 rounded-2xl backdrop-blur-sm min-h-[500px]  ${changeBg()}`}
+          <div className={`text-3xl sm:col-span-full md:col-span-4`}>
+            <div
+              className={` grid grid-cols-1 grid-row-2  sm:gap-2 md:gap-3 lg:gap-6 `}
+            >
+              <div
+                className={`rounded-2xl  backdrop-blur-sm min-h-[200px] ${changeBg()}`}
+              >
+                gri-5
+              </div>
+              <div
+                className={` grid sm:grid-cols-1 md:grid-cols-2 min-h-[200px] sm:gap-2 md:gap-3 lg:gap-6 `}
+              >
+                <div
+                  className={` backdrop-blur-sm rounded-2xl sm:min-h-[200px] md:min-h-max ${changeBg()}`}
+                ></div>
+                <div
+                  className={` backdrop-blur-sm rounded-2xl sm:min-h-[200px] md:min-h-max ${changeBg()}`}
+                ></div>
+              </div>
+            </div>
+          </div>
+          {/* <div
+            className={`text-3xl sm:col-span-full md:col-span-3 rounded-2xl backdrop-blur-sm min-h-[350px]  ${changeBg()}`}
           >
-            grid 2
-          </div>
-          <div
-            className={`text-3xl sm:col-span-full md:col-span-3 rounded-2xl backdrop-blur-sm min-h-[500px]  ${changeBg()}`}
-          >
-            grid 3
-          </div>
+            grid 6
+          </div> */}
         </div>
       </div>
     </div>
